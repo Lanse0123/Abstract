a33b33cf67cbbb9ae3f097908bcf65384038c99b
Lanse <lanse250@gmail.com>
2025-05-26 17:26:38 -0400

    I removed the old icon matcher thing and added a new set of instructions for Qwertious to follow to help make a list of all their languages and icons. Also reordered some of those files and folder structures

---
+++ src/main/java/lanse/abstractt/storage/languages/LanguageManager.java
@@ -0,0 +1,16 @@
+package lanse.abstractt.storage.languages;
+
+public class LanguageManager {
+
+    //TODO - Things to list for the language:
+    // What is a Comment (list) ((for example, java has // and /* and /** and */))
+    // What is a function
+    // What is the default Main of the project (can be set by user though)
+    // What is it's Icon
+    // Exceptions or things to note. For example: SAS uses input cards; ; for Data, and that could probably be represented as
+    // bubble instead of ignored.
+    // Whether to store the language as a JSON or a Class. (Dont worry too much about this, but if it has a lot of exceptions
+    // or stuff that make it harder, then it might need to use code to run. For example, Javascript's function is simply function,
+    // while java is a regex of () {}
+
+}
