005c911ea4739897203ea732f91df3e062bb3ba2
Lanse <lanse250@gmail.com>
2025-05-27 17:14:07 -0400

    Added MORE stuff to Credit screen

--- src/main/java/lanse/abstractt/core/screens/CreditScreen.java
+++ src/main/java/lanse/abstractt/core/screens/CreditScreen.java
@@ -23,28 +23,28 @@ public class CreditScreen extends JPanel {
 
         //TODO - add text next to icon
         ImageIcon lanseLogoIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/credits/LanseIcon.png")));
-        Image image = lanseLogoIcon.getImage().getScaledInstance(187, 151, Image.SCALE_SMOOTH);
+        Image image = lanseLogoIcon.getImage().getScaledInstance(161, 151, Image.SCALE_SMOOTH);
         lanseLogoIcon = new ImageIcon(image);
         JLabel logo = new JLabel(lanseLogoIcon);
         logo.setAlignmentX(LEFT_ALIGNMENT);
 
         //TODO - add text next to icon
-        ImageIcon jamesLogoIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/credits/LanseIcon.png")));
-        Image image2 = jamesLogoIcon.getImage().getScaledInstance(187, 151, Image.SCALE_SMOOTH);
+        ImageIcon jamesLogoIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/credits/JamesIcon.png")));
+        Image image2 = jamesLogoIcon.getImage().getScaledInstance(146, 146, Image.SCALE_SMOOTH);
         jamesLogoIcon = new ImageIcon(image2);
         JLabel logo2 = new JLabel(jamesLogoIcon);
         logo2.setAlignmentX(LEFT_ALIGNMENT);
 
         //TODO - add text next to icon
         ImageIcon ytIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/credits/ytIcon.png")));
-        Image image3 = ytIcon.getImage().getScaledInstance(187, 131, Image.SCALE_SMOOTH);
+        Image image3 = ytIcon.getImage().getScaledInstance(168, 116, Image.SCALE_SMOOTH);
         ytIcon = new ImageIcon(image3);
         JLabel logo3 = new JLabel(ytIcon);
         logo3.setAlignmentX(LEFT_ALIGNMENT);
 
         //TODO - add text next to icon
         ImageIcon discordIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/credits/discordIcon.png")));
-        Image image4 = discordIcon.getImage().getScaledInstance(187, 121, Image.SCALE_SMOOTH);
+        Image image4 = discordIcon.getImage().getScaledInstance(170, 111, Image.SCALE_SMOOTH);
         discordIcon = new ImageIcon(image4);
         JLabel logo4 = new JLabel(discordIcon);
         logo4.setAlignmentX(LEFT_ALIGNMENT);
@@ -58,7 +58,7 @@ public class CreditScreen extends JPanel {
         add(logo);
         add(Box.createRigidArea(new Dimension(0, 0)));
         add(logo2);
-        add(Box.createRigidArea(new Dimension(0, 5)));
+        add(Box.createRigidArea(new Dimension(0, 10)));
         add(logo3);
         add(Box.createRigidArea(new Dimension(0, 15)));
         add(logo4);
