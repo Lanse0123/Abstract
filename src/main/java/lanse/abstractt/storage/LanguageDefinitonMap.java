65fe6558978c6a00742ef07116365ee1a16c06eb
Lanse <lanse250@gmail.com>
2025-05-30 16:18:37 -0400

    Part 1 of refactoring all the jsons to use extension name instead of full name for easy access

--- src/main/java/lanse/abstractt/storage/LanguageDefinitonMap.java
+++ src/main/java/lanse/abstractt/storage/LanguageDefinitonMap.java
@@ -5,5 +5,6 @@ public class LanguageDefinitonMap {
     //TODO - this will store as many languages as it can, as well as behavior that class uses.
     // For example, I might need to know what the languages uses for comments, or a simple thing like
     // function definitions, or even if its object oriented or whatever just to know how to handle it.
+    // This is really just for languages too hard to store in JSON
 
 }
--- src/main/java/lanse/abstractt/storage/languages/LanguageManager.java
+++ src/main/java/lanse/abstractt/storage/languages/LanguageManager.java
@@ -1,16 +1,29 @@
 package lanse.abstractt.storage.languages;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class LanguageManager {
 
-    //TODO - Things to list for the language:
-    // What is a Comment (list) ((for example, java has // and /* and /** and */))
-    // What is a function
-    // What is the default Main of the project (can be set by user though)
-    // What is it's Icon
-    // Exceptions or things to note. For example: SAS uses input cards; ; for Data, and that could probably be represented as
-    // bubble instead of ignored.
-    // Whether to store the language as a JSON or a Class. (Dont worry too much about this, but if it has a lot of exceptions
-    // or stuff that make it harder, then it might need to use code to run. For example, Javascript's function is simply function,
-    // while java is a regex of () {}
+    public static Image getImageFromLanguage(String extension) {
+        String basePath = "/resources/images/LanguageIcons/";
 
+        if (extension.equals("folder")) {
+            try {
+                return new ImageIcon(LanguageManager.class.getResource(basePath + "DefaultDirectory.png")).getImage();
+            } catch (Exception e) {
+                try {
+                    return new ImageIcon(LanguageManager.class.getResource(basePath + "DefaultFile.png")).getImage();
+                } catch (Exception ignored) {}
+            }
+        }
+        try {
+            return new ImageIcon(LanguageManager.class.getResource(basePath + extension + ".png")).getImage();
+        } catch (Exception e) {
+            try {
+                return new ImageIcon(LanguageManager.class.getResource(basePath + "DefaultFile.png")).getImage();
+            } catch (Exception ignored) {}
+        }
+        return null;
+    }
 }
