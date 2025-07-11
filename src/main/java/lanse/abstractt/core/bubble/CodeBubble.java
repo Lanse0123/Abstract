35369b2d2a32c56dbd7d42308a7fd61f1c8a4d8e
Lanse <lanse250@gmail.com>
2025-07-10 20:54:44 -0400

    Preparing CodeBubbles and making the rectangle

--- src/main/java/lanse/abstractt/core/bubble/CodeBubble.java
+++ src/main/java/lanse/abstractt/core/bubble/CodeBubble.java
@@ -1,7 +1,12 @@
 package lanse.abstractt.core.bubble;
 
+import lanse.abstractt.core.ColorPalette;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Rectangle2D;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -9,7 +14,36 @@ import java.io.IOException;
 
 public class CodeBubble extends JPanel {
 
+    protected String fileContents;
+    protected Color color;
+    protected int width = 680;
+    protected int height = 360;
+
     //TODO - this will display the code in a rectangle bubble next to the description bubbles.
+
+    public CodeBubble(String fileContents) {
+        this.fileContents = fileContents;
+        this.color = new Color(30, 30, 30);
+
+        setPreferredSize(new Dimension(width, height));
+        setOpaque(false);
+        setLayout(new BorderLayout());
+        initUI();
+
+        //Click handler
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+                //TODO - logic for clicking and editing the text can go here.
+
+                Container parent = getParent();
+
+                parent.revalidate();
+                parent.repaint();
+            }
+        });
+    }
 
     public static void createCodeBubble(String filePath, Container parent){
 
@@ -19,11 +53,13 @@ public class CodeBubble extends JPanel {
             return;
         }
 
-        java.util.List<String> prompts = new java.util.ArrayList<>();
+        //TODO - get the widest line, and get how many lines tall it is. This will be used later in the constructor.
+        // also copy everything into fileContents
+
+        String fileContents = "";
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             StringBuilder currentBlock = new StringBuilder();
             int lineNumber = 1;
-            int maxChars = 200;
             String line;
 
             while ((line = reader.readLine()) != null) {
@@ -32,25 +68,87 @@ public class CodeBubble extends JPanel {
                     continue;
                 }
 
-                String lineWithNumber = lineNumber + ": " + line + "\n";
+                String lineWithNumber = lineNumber + ".     " + line + "\n";
 
-                if (currentBlock.length() + lineWithNumber.length() > maxChars) {
-                    prompts.add(currentBlock.toString());
-                    currentBlock.setLength(0);
-                }
+                //fileContents.add(currentBlock.toString());
+                currentBlock.setLength(0);
 
                 currentBlock.append(lineWithNumber);
                 lineNumber++;
             }
 
             if (!currentBlock.isEmpty()) {
-                prompts.add(currentBlock.toString());
+                //fileContents.add(currentBlock.toString());
             }
 
         } catch (IOException e) {
             System.err.println("Failed to read file: " + filePath);
             e.printStackTrace();
             return;
+        }
+
+        CodeBubble codeBubble = new CodeBubble(fileContents);
+        parent.add(codeBubble);
+    }
+
+    protected void initUI() {
+
+        //TODO - remake this for text and new format
+
+        // CENTER: title + description
+        JPanel centerPanel = new JPanel();
+        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+        centerPanel.setOpaque(false);
+
+        centerPanel.add(Box.createVerticalStrut(10));
+        centerPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+
+        // RIGHT placeholder
+        JPanel rightPanel = new JPanel();
+        rightPanel.setPreferredSize(new Dimension(30, 30));
+        rightPanel.setOpaque(false);
+
+        JPanel leftWrapper = new JPanel(new BorderLayout());
+        leftWrapper.setOpaque(false);
+
+        add(leftWrapper, BorderLayout.WEST);
+        add(centerPanel, BorderLayout.CENTER);
+        add(rightPanel, BorderLayout.EAST);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Define rectangle shape and clip to it
+        //TODO - make sure this is the size / shape that we define it as based on the line counter thingy
+        Shape rectangle = new Rectangle2D.Double(0, 0, getWidth(), getHeight());
+        g2.setClip(rectangle);
+
+        if (color != null && color != Color.BLACK){
+            g2.setColor(color);
+        } else {
+            g2.setColor(ColorPalette.ColorCategory.BUBBLES_AND_PROGRESS.getColor());
+        }
+
+        g2.fill(rectangle);
+
+        // Draw outline
+        g2.setColor(ColorPalette.ColorCategory.OUTLINE.getColor());
+        g2.setStroke(new BasicStroke(8));
+        g2.draw(rectangle);
+
+        g2.dispose();
+        super.paintComponent(g);
+    }
+
+    @Override
+    public void setSize(int width, int height) {
+        if (this.width != width || this.height != height) {
+            this.width = width;
+            this.height = height;
+            setPreferredSize(new Dimension(width, height));
         }
     }
 }
