1f8fb2beccc261893b943196e69c8ccde7402159
Lanse <lanse250@gmail.com>
2025-07-26 12:29:15 -0400

    Fixed TODO sort for function bubbles

--- src/main/java/lanse/abstractt/core/bubblesortlogic/TODOViewSort.java
+++ src/main/java/lanse/abstractt/core/bubblesortlogic/TODOViewSort.java
@@ -29,9 +29,7 @@ public class TODOViewSort {
                 String filePath = bubble.getFilePath();
                 File file = new File(filePath);
 
-                long size = getPathSize(file); // Handles files and directories
-
-                bubble.scale = 1 + Math.log(size + 1);
+                bubble.scale = 1 + Math.log(getPathSize(file) + 1);
             }
         } else {
             for (Bubble bubble : bubbles) {
@@ -40,7 +38,6 @@ public class TODOViewSort {
                     File file = new File(filePath);
 
                     try {
-                        //TODO - this code could also just about be used to make a CodeBubble of a FunctionBubble
                         List<String> lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
                         int[] span = bubble1.getLineSpan();
                         int startLine = span[0];
@@ -48,13 +45,16 @@ public class TODOViewSort {
 
                         if (startLine == -40404 || endLine == -40404) return null;
 
-                        int byteCount = 0;
-                        for (int i = startLine; i < endLine; i++) {
-                            byteCount += lines.get(i).getBytes(StandardCharsets.UTF_8).length;
+                        int todoCount = 0;
+                        for (int i = startLine; i < endLine && i < lines.size(); i++) {
+                            String line = lines.get(i);
+                            int index = -1;
+                            while ((index = line.indexOf("TODO", index + 1)) != -1) {
+                                todoCount++;
+                            }
                         }
 
-                        double sizeInKB = byteCount / 1024.0;
-                        bubble.scale = 1 + Math.log(sizeInKB + 1);
+                        bubble.scale = 1 + Math.log(todoCount + 1);
 
                     } catch (IOException e) {
                         System.out.println("Error calculating file size in SizeSort");
