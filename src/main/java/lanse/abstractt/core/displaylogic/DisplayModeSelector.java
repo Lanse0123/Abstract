6a1eb51a45bf6f02e91fec751a0378f021539ee7
Lanse <lanse250@gmail.com>
2025-07-26 20:34:17 -0400

    Part 3 of making static maps work

--- src/main/java/lanse/abstractt/core/displaylogic/DisplayModeSelector.java
+++ src/main/java/lanse/abstractt/core/displaylogic/DisplayModeSelector.java
@@ -4,7 +4,6 @@ import lanse.abstractt.core.WorldMap;
 import lanse.abstractt.core.bubble.Bubble;
 
 import java.awt.*;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -14,11 +13,9 @@ public class DisplayModeSelector {
         FILE_LIST, MAIN_LIST_DOWN, PROXIMITY_SIMULATOR, ANTI_COLLIDER, TODO_VIEW, GOURCE_MAP
     }
 
-    //TODO - These will be the maps that are generated at runtime instead of
-    // from users clicking bubbles. (dont do this yet)
-    public static List staticMapList = List.of(new List[]{
-            Collections.singletonList(DisplayMode.GOURCE_MAP)
-    });
+    public static List<DisplayMode> staticMapList = List.of(
+            DisplayMode.GOURCE_MAP
+    );
 
     public static DisplayMode displayMode = DisplayMode.FILE_LIST;
 
